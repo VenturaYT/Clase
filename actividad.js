43b31a7f1f0dacfd8f55ba6e823b7672e3d87dd0
VenturaYT <vicyyat@gmail.com>
2021-09-25 15:14:56 -0500

    25/09/2021
    Clase
    Tarea tabla de multiplicar
    Gasolina

---
+++ actividad.js
@@ -0,0 +1,8 @@
+const numBase = 5
+let numLimit = 0
+let result
+
+for (numLimit; numLimit <= 10; numLimit++) {
+    result = numBase * numLimit
+    console.log('El resultado de multiplicar ' + numBase + ' x ' + numLimit + ' es: ' + result)
+}
---
+++ actividad2.js
@@ -0,0 +1,9 @@
+gasolina = 100;
+
+while (gasolina > 0) {
+
+    gasolina--;
+    console.log("Gasolina Restante: " + gasolina)
+
+}
+console.log("La gasolina se ha terminado")
